ae7f97715469628a59408ba80aa93ded5f4694e8
Niteify <40525844+Niteify@users.noreply.github.com>
2022-09-26 18:55:50 -0400

    i'll get this workin
    i'll get this workin

--- server.mjs
+++ server.mjs
@@ -7,9 +7,9 @@ const serveHtml = serveStatic('.', {'index': ['index.html']});
 
 const server = http.createServer((req, res) => {
     if (req.url === '/get-small-background-images-paths') {
-        res.end(JSON.stringify(fs.readdirSync('images/background/small').map(path => 'images/background/small/' + path)));
+        res.end(JSON.stringify(fs.readdirSync('nw-crest-designer/images/background/small').map(path => 'nw-crest-designer/images/background/small/' + path)));
     } else if (req.url === '/get-small-foreground-images-paths') {
-        res.end(JSON.stringify(fs.readdirSync('images/foreground/small').map(path => 'images/foreground/small/' + path)));
+        res.end(JSON.stringify(fs.readdirSync('nw-crest-designer/images/foreground/small').map(path => 'nw-crest-designer/images/foreground/small/' + path)));
     } else {
         serveHtml(req, res, finalhandler(req, res));
     }
